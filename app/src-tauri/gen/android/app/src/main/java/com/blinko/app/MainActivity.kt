7cde2554cb46362acb8d125224ab309967ab473a
Blinko <blinkospace@gmail.com>
2025-04-21 20:32:52 +0800

    update

--- app/src-tauri/gen/android/app/src/main/java/com/blinko/app/MainActivity.kt
+++ app/src-tauri/gen/android/app/src/main/java/com/blinko/app/MainActivity.kt
@@ -1,3 +1,37 @@
 package com.blinko.app
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import android.graphics.Color
+import android.content.res.Configuration
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        updateColorsBasedOnTheme()
+    }
+    
+    private fun isDarkTheme(): Boolean {
+        return resources.configuration.uiMode and 
+                Configuration.UI_MODE_NIGHT_MASK == 
+                Configuration.UI_MODE_NIGHT_YES
+    }
+    
+    private fun updateColorsBasedOnTheme() {
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        
+        if (isDarkTheme()) {
+            window.statusBarColor = Color.BLACK
+            window.navigationBarColor = Color.BLACK
+            // 深色模式下使用浅色图标
+            controller.isAppearanceLightStatusBars = false
+            controller.isAppearanceLightNavigationBars = false
+        } else {
+            window.statusBarColor = Color.WHITE
+            window.navigationBarColor = Color.WHITE
+            // 浅色模式下使用深色图标
+            controller.isAppearanceLightStatusBars = true
+            controller.isAppearanceLightNavigationBars = true
+        }
+    }
+}
